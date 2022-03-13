2527b852842502e714e497c379dc0df15cb1c20b
khalil <khalillakhdharatc@gmail.com>
2022-03-13 11:20:07 +0100

    Create Bidul.java

---
+++ src/main/java/com/duranco/model/Bidul.java
@@ -0,0 +1,10 @@
+package com.duranco.model;
+
+class Machin { 
+public int f() {return(5) }; 
+
+public static int g() {return (6);}
+class Bidul extends Machin {
+public int f() {return(2) };
+public static int g() {return (4); }
+} }
